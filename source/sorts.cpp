62152bcda8bdcb2eab339f505773b5808e19f6c5
URI <URI@150.156.20.172.s.wireless.uri.edu>
2019-06-05 16:46:32 -0400

    lab/selection

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -18,9 +18,40 @@ void bubble_sort(int* arr, int n) {
 }
 
 /** Selection Sort
- * O(?), Omega(?), Theta(?)
+ * O(n^2), Omega(n^2), Theta(n^2)
  */
 
+void swap(int* x, int* y) {
+  int z;
+  z = *x;
+  *x = *y;
+  *y = z;
+}
+
+void selection_sort(int* arr, int n) {
+  int min;
+  for (int i = 0; i < n - 1; i++) {
+    min = i;
+    for (int j = i + 1; j < n; j++) {
+      if (arr[j] < arr[min]) {
+        min = j;
+      }
+    }
+    swap(&arr[i], &arr[min]);
+  }
+}
+
 /** Insertion Sort
- * O(?), Omega(?), Theta(?)
+ * O(n^2), Omega(kn), Theta(n)
  */
+void insertion_sort(int* arr, int n) {
+  for (int i = 1; i < n; i++) {
+    for (int j = i; j >= 0; j--) {
+      if (arr[j - 1] > arr[j]) {
+        swap(&arr[j - 1], &arr[j]);
+      } else {
+        break;
+      }
+    }
+  }
+}
