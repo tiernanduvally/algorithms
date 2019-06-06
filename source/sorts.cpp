98796ab07d0c30535b978651e5a1be46076b48eb
Tiernan Duvally <tiernan_duvally@my.uri.exu>
2019-06-06 15:17:21 -0400

    merge

--- source/sorts.cpp
+++ source/sorts.cpp
@@ -25,75 +25,10 @@ void bubble_sort(int* arr, int n) {
 /** Insertion Sort
  * O(?), Omega(?), Theta(?)
  */
-=======
-
-void swap(int * y, int * x) {
-  int temp;
-  temp = *y;
-  *y = *x;
-  *x = temp;
-}
-
-void selection_sort(int* arr, int n) {
- 
-  for(int i = 0; i < (n-1); i++) {
-    int min = i;
-    for (int j = i + 1; j < n ; j++) {
-        if(arr[j] < arr[min]) min = j;
-    }
-    swap(&arr[min],&arr[i]);
-    
-  }
-}
-
-void insertion_sort(int* arr, int n) {
-    for(int i = 0; i < n; i++) {
-      // Set j = i 
-      // j > 0 -> for each j from i to 0
-      // if the element at j-1 is greater than the element at j 
-      // -> arr[j] < arr[j-1]
-      for(int j = i; j > 0 && arr[j] < arr[j-1]; j--) {
-        //then swap the elements
-        swap(&arr[j],&arr[j-1]);
-      }
-    }
-
-}
-
-
-
-
-
-
-/** Selection Sort
- * O(n^2), Omega(n^2), Theta(n^2)
- */
-void selection_sort(int * arr, int n)
-{
-  int min;
-  for(int i = 0; i < (n - 1); i++)
-  {
-    min = i;
-    for(int j = i+1; j < n ; j++)
-    {
-      if(arr[j]<arr[min])
-      {
-        min = j;
-      }
-    }
-    int temp = arr[i];
-    arr[i] = arr[min];
-    arr[min]=temp;
-  }
-}
-/** Insertion Sort
- * O(n^2), Omega(n), Theta(N/A) Average(n^2)
- * 
- */
 
 void insertion_sort(int* arr, int n)
  {
-   
+
    int min;
   for(int i =0; i < n; i++)
   {
